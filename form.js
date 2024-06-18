18a8a841b1d6fe163703732e74766068fa8c8dbc
Olesya Krayanskaya <krayanskaya_ov@mail.ru>
2024-06-19 00:15:36 +0300

    send data

--- form.js
+++ form.js
@@ -11,5 +11,26 @@ function inputValidate() {
     })
 }
 
+const form = document.getElementById('formId')
+
+form.onsubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(form)
+
+    formData.set("org", formData.get("org") + "aaaaaaaaaaa" )
+
+    for (var [key, value] of formData.entries()) { 
+        console.log(key, value);
+      }
+
+    let response = await fetch('', {
+      method: 'POST',
+      body: formData
+    });
+
+    let result = await response.json();
+
+  };
+
 // const sendBtn = document.getElementById('submit');
 // sendBtn.addEventListener('click', inputValidate());
