2dd9e0bbd677252eeef8038ef0d5b51e4f30a863
Olesya <krayanskaya_ov@mail.ru>
2024-07-16 14:45:22 +0300

    merge

--- form.js
+++ form.js
@@ -1,15 +1,2 @@
 'use strict'
 
-function inputValidate() {
-    const inputs = document.querySelectorAll('.form__input');
-    const errMsg = document.querySelector('.error-msg');
-
-    inputs.forEach((input) => {
-        if (!input.checkValidity()) {
-            errMsg.innerHTML = input.validationMessage;
-        }
-    })
-}
-
-// const sendBtn = document.getElementById('submit');
-// sendBtn.addEventListener('click', inputValidate());
